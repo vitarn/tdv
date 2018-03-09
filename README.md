f0633fc979c127e9fc05665dd05d4c5693b7062b
Colder Xihk <cncolder@users.noreply.github.com>
2018-03-09 13:06:58 +0800

    feat: first version (#1)
    * feat: first version
    
    * fix: limit joi version <= 12

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
 # tdv
+
+[![License][ico-license]][link-license]
+[![NPM][ico-npm]][link-npm]
+[![Build Status][ico-build]][link-build]
+[![Coverage Status][ico-codecov]][link-codecov]
+
+Typescript (definition|decorator) validator base on Joi
+
+## Example
+
+`tsconfig.json`
+```json
+{
+    "compilerOptions": {
+        "experimentalDecorators": true,
+        "emitDecoratorMetadata": true,
+    },
+}
+```
+
+```ts
+import { Schema, required, optional } from 'tdv'
+
+class User extends Schema {
+    @required
+    id: number
+
+    @required(Joi => Joi.string().email())
+    email: string
+
+    @required
+    profile: Profile
+}
+
+class Profile extends Schema {
+    @optional
+    displayName?: string
+}
+
+const user = new User({ id: 123, email: 'foo@example.com', profile: { displayName: 'Joe' } })
+
+console.log(user.validate())
+{ error: null,
+    value: { id: 123, email: 'foo@example.com', profile: { displayName: 'Joe' } },
+    then: [Function: then],
+    catch: [Function: catch] }
+
+console.log(user.attempt())
+{ id: 123, email: 'foo@example.com', profile: { displayName: 'Joe' } }
+
+console.log(user.toJSON())
+{ id: 123, email: 'foo@example.com', profile: { displayName: 'Joe' } }
+
+const user2 = new User({})
+
+console.log(user2.validate())
+{ error:
+        { ValidationError: child "id" fails because ["id" is required]
+        at error stack...
+        isJoi: true,
+        name: 'ValidationError',
+        details: [ [Object] ],
+        _object: { id: undefined, profile: undefined },
+        annotate: [Function] },
+    value: { id: undefined, profile: undefined },
+    then: [Function: then],
+    catch: [Function: catch] }
+
+console.log(user.attempt())
+{ ValidationError: {
+    "id" [1]: -- missing --
+    }
+
+    [1] "id" is required
+        at error stack...
+    isJoi: true,
+    name: 'ValidationError',
+    details:
+        [ { message: '"id" is required',
+            path: [Array],
+            type: 'any.required',
+            context: [Object] } ],
+    _object: { id: undefined, profile: undefined },
+    annotate: [Function] }
+
+console.log(user.toJSON())
+{ id: undefined, profile: undefined }
+```
+
+[ico-license]: https://img.shields.io/github/license/vitarn/tdv.svg
+[ico-npm]: https://img.shields.io/npm/v/tdv.svg
+[ico-build]: https://travis-ci.org/vitarn/tdv.svg?branch=master
+[ico-codecov]: https://codecov.io/gh/vitarn/tdv/branch/master/graph/badge.svg
+
+[link-license]: ./blob/master/LICENSE
+[link-npm]: https://www.npmjs.com/package/tdv
+[link-build]: https://travis-ci.org/vitarn/tdv
+[link-codecov]: https://codecov.io/gh/vitarn/tdv
---
+++ index.test.ts
@@ -0,0 +1,131 @@
+import { Schema, required, optional, $metadata, $validator } from './'
+
+
+describe('Schema', () => {
+    describe('$metadata', () => {
+        class FirstSchema extends Schema {
+            @required(Joi => Joi.string().uuid({ version: 'uuidv4' }))
+            id: string
+
+            @optional(j => j.string())
+            name?: string
+        }
+
+        class SecondSchema extends FirstSchema {
+
+        }
+
+        class ThirdSchema extends SecondSchema {
+            @required(j => j.string().min(5).max(40))
+            name: string
+
+            @optional(j => j.number().min(1).max(199))
+            age?: number
+
+            @optional
+            active?: boolean
+        }
+
+        it('has no metadata in Schema', () => {
+            expect(Object.keys(Schema[$metadata])).toEqual([])
+        })
+
+        it('have metadatas in FirstSchema', () => {
+            expect(Object.keys(FirstSchema[$metadata])).toEqual(['id', 'name'])
+        })
+
+        it('have metadatas in SecondSchema same as FirstSchema', () => {
+            expect(Object.keys(SecondSchema[$metadata])).toEqual(['id', 'name'])
+        })
+
+        it('have metadatas in ThirdSchema', () => {
+            expect(Object.keys(ThirdSchema[$metadata])).toEqual(['id', 'name', 'age', 'active'])
+        })
+    })
+
+    describe('toJSON', () => {
+        class User extends Schema {
+            @required id: number
+            @required profile: Profile
+        }
+
+        class Profile extends Schema {
+            @required name: string
+        }
+
+        it('output json include refs', () => {
+            let user = new User({
+                id: 1,
+                profile: new Profile({ name: 'Joe' })
+            })
+
+            expect(user.toJSON()).toEqual({
+                id: 1,
+                profile: {
+                    name: 'Joe',
+                },
+            })
+        })
+    })
+
+    describe('validate', () => {
+        class Profile extends Schema {
+            @required(j => j.string())
+            name: string
+        }
+
+        class User extends Schema {
+            @required(j => j.number())
+            id: number
+
+            @required
+            profile: Profile
+        }
+
+        it('contain error if invalid', () => {
+            let user = new User({ id: 'abc', profile: new Profile({ name: 'Joe' }) })
+
+            expect(user.validate().error).toBeTruthy()
+        })
+
+        it('return null error if valid', () => {
+            let user = new User({ id: '123', profile: new Profile({ name: 'Joe' }) })
+
+            expect(user.validate().error).toBeNull()
+        })
+    })
+
+    describe('attempt', () => {
+        class Profile extends Schema {
+            @required(j => j.string())
+            name: string
+        }
+
+        class User extends Schema {
+            @required(j => j.number())
+            id: number
+
+            @required
+            profile: Profile
+        }
+
+        it('throw error if invalid', () => {
+            let user = new User({ id: 'abc', profile: new Profile({ name: 'Joe' }) })
+
+            expect(() => user.attempt()).toThrow()
+        })
+
+        it('return json if valid', () => {
+            let user = new User({ id: '123', profile: new Profile({ name: 'Joe' }) })
+
+            expect(user.attempt()).toEqual({
+                id: 123,
+                profile: {
+                    name: 'Joe',
+                },
+            })
+
+            console.log(new User().toJSON())
+        })
+    })
+})
